93a62fb39a325257e1ddc154b9ca86fe76768ff1
mehedi-hasan02 <mehedisani02@gmail.com>
2024-06-01 11:53:50 +0600

    implement meals by category section

--- src/Pages/Home/MealsCategory/MealsCategory.jsx
+++ src/Pages/Home/MealsCategory/MealsCategory.jsx
@@ -5,23 +5,38 @@ import MealTab from './MealTab/MealTab';
 
 const MealsCategory = () => {
     const [meals] = useMeal();
+    const breakfast = meals.filter(meal => meal.category === 'Breakfast');
+    const lunch = meals.filter(meal => meal.category === 'Lunch');
+    const dinner = meals.filter(meal => meal.category === 'Dinner');
 
     return (
-        <Tabs>
-            <TabList>
-                <Tab>All Meals</Tab>
-                <Tab>Breakfast</Tab>
-                <Tab>Lunch</Tab>
-                <Tab>Dinner</Tab>
-            </TabList>
+        <div>
+            <div className='text-center my-14 space-y-3'>
+                <h2 className='text-4xl font-bold'>Meals by Category</h2>
+                <p className='w-3/4 mx-auto'>Browse our tasty meal options by category: Breakfast, Lunch, Dinner, or All Meals. Each card includes an image, rating, price, and a details button for more information. Enjoy a convenient and delightful dining experience!</p>
+            </div>
+            <Tabs>
+                <TabList>
+                    <Tab>All Meals</Tab>
+                    <Tab>Breakfast</Tab>
+                    <Tab>Lunch</Tab>
+                    <Tab>Dinner</Tab>
+                </TabList>
 
-            <TabPanel>
-                <MealTab meals={meals}/>
-            </TabPanel>
-            <TabPanel>
-                <h2>Any content 2</h2>
-            </TabPanel>
-        </Tabs>
+                <TabPanel>
+                    <MealTab meals={meals} />
+                </TabPanel>
+                <TabPanel>
+                    <MealTab meals={breakfast} />
+                </TabPanel>
+                <TabPanel>
+                    <MealTab meals={lunch} />
+                </TabPanel>
+                <TabPanel>
+                    <MealTab meals={dinner} />
+                </TabPanel>
+            </Tabs>
+        </div>
     );
 };
 
--- src/Pages/Shared/MealCard/MealCard.jsx
+++ src/Pages/Shared/MealCard/MealCard.jsx
@@ -3,16 +3,15 @@
 const MealCard = ({ meal }) => {
     return (
         <div className="card bg-base-100 shadow-xl">
-            <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <figure><img src={meal.image} alt="Meals" /></figure>
             <div className="card-body">
                 <h2 className="card-title">
-                    Shoes!
-                    <div className="badge badge-secondary">NEW</div>
+                    {meal.name}
                 </h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <p>${meal.price}</p>
                 <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
+                    {/* <div className="badge badge-outline">Fashion</div> */}
+                    <button className="badge badge-outline">Details</button>
                 </div>
             </div>
         </div>
